b69ce77bffab8b7b6f1da50055c0fc07be0365aa
yWillRJ <xextremojuniorx@gmail.com>
2023-09-15 01:38:15 -0300

    minha pombaaaaaaaaaa

---
+++ xvidros.js
@@ -0,0 +1,47 @@
+var a1 = document.querySelectorAll(".videor")
+
+a1.forEach((a5) => {
+    a5.addEventListener("mouseenter", () => {
+        a5.src = "/jailsongif.gif"
+        setTimeout(() => {
+            a5.src = "/screenshot_24.png"
+        }, 20000);
+    })
+    a5.addEventListener("mouseleave", () => {
+        a5.src = "/screenshot_24.png"
+    })
+})
+var a2 = document.querySelectorAll(".videow")
+
+a2.forEach((a5) => {
+    a5.addEventListener("mouseenter", () => {
+        a5.src = "macacogif.gif"
+        setTimeout(() => {
+            a5.src = "/image.png"
+        }, 20000);
+    })
+    a5.addEventListener("mouseleave", () => {
+        a5.src = "/image.png"
+    })
+})
+var a3 = document.querySelectorAll(".videom")
+
+a3.forEach((a5) => {
+    a5.addEventListener("mouseenter", () => {
+        a5.src = "gifj.gif"
+        setTimeout(() => {
+            a5.src = "/mohamed.png"
+        }, 20000);
+    })
+    a5.addEventListener("mouseleave", () => {
+        a5.src = "/mohamed.png"
+    })
+})
+
+
+
+
+
+// a5.src=`
+// https://el.phncdn.com/gif/45027791.gif
+// `
